a5d2dda2be2cc08faa9d0a4cc3e4c4f1b899a3e7
Piotr Halicki <Piotr_Halicki@wp.pl>
2015-03-23 20:36:41 +0100

    Początek

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html lang="pl-PL">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="Ćwiczenia - mySQL - Kino" content="ćwiczenia, mySQL, kino">	
+		<title>						
+		Ćwiczenia - mySQL - Kino
+		</title>
+	</head>
+
+	<body>
+
+	<h1>
+	<strong><em>Ćwiczenia - mySQL - Kino</em></strong>
+	</h1>
+	
+		<br>
+	<hr>
+		<br>
+
+
+<?php
+
+
+
+?>
+
+
+	</body>
+
+</html>	
